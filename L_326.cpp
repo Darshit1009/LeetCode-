9d2ed46da9eff9e39cbd4eea0eac6ae3dabb9328
Darshit1009 <darshitkacha1009@gmail.com>
2025-10-11 19:34:27 +0530

    find given number is power of Three or not

---
+++ L_326.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+bool isPowerOfThree(int n)
+{
+    if (n <= 0)
+    {
+        return false;
+    }
+    while (n % 3 == 0)
+    {
+        n = n / 3;
+    }
+    return n == 1;
+}
+int main(int argc, char const *argv[])
+{
+    cout << isPowerOfThree(9);
+    return 0;
+}
