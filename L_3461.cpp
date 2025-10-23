d762fede9fcefbab8f6b031be454c23d60f40340
Darshit1009 <darshitkacha1009@gmail.com>
2025-10-23 23:40:18 +0530

    in  given string check the all digit of  number is same or not

---
+++ L_3461.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool hasSameDigits(string s)
+    {
+        while (s.size() > 2)
+        {
+            string next;
+            for (int i = 0; i < s.size() - 1; i++)
+            {
+                int a = s[i] - '0';
+                int b = s[i + 1] - '0';
+                next += ((a + b) % 10) + '0';
+            }
+            s = next;
+        }
+        return s[0] == s[1];
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.hasSameDigits("11");
+
+    return 0;
+}
