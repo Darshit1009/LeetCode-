d66e46fcbac5376a29b2c0fdfc559bd723b71664
Darshit1009 <darshitkacha1009@gmail.com>
2025-10-25 15:13:26 +0530

    calculate the total sum which has exactly four divisor in vector

---
+++ L_1390.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int sumFourDivisors(vector<int> &nums)
+    {
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int count = 0;
+            int sum = 0;
+
+            for (int j = 1; j <= nums[i]; j++)
+            {
+                if (nums[i] % j == 0)
+                {
+                    ++count;
+                    sum += j;
+                    if (count > 4)
+                        break;
+                }
+            }
+
+            if (count == 4)
+            {
+                totalSum += sum;
+            }
+        }
+
+        return totalSum;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> d = {21, 4, 7};
+    cout << s.sumFourDivisors(d);
+
+    return 0;
+}
