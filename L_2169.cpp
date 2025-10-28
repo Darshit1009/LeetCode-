dacdd33f31e28ddbd94bc76fc317afb17059e1d3
Darshit1009 <darshitkacha1009@gmail.com>
2025-10-28 19:02:23 +0530

    count operation to obtain zero

---
+++ L_2169.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    int countOperations(int num1, int num2)
+    {
+        int count = 0;
+        while (num1 != 0 && num2 != 0)
+        {
+            if (num1 > num2)
+            {
+                num1 = num1 - num2;
+            }
+            else
+                num2 = num2 - num1;
+            count++;
+        }
+        return count;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    cout << s.countOperations(2, 3);
+    return 0;
+}
