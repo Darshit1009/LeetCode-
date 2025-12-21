8638ec163ea221b0785d0aa86f6238aa8f3defe0
Darshit1009 <darshitkacha1009@gmail.com>
2025-12-21 17:50:44 +0530

    Valid Parentheses

---
+++ L_20.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+#include <string>
+#include <iostream>
+using namespace std;
+bool isValidParentheses(const string &s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '(' || c == '{' || c == '[')
+        {
+            st.push(c);
+        }
+        else
+        {
+            if (st.empty())
+                return false;
+            char top = st.top();
+            st.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '['))
+            {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+
+int main()
+{
+    cout << isValidParentheses("()]");
+    return 0;
+}
