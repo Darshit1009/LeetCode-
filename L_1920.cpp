7e344b9a28f8d4be236bd3e78a06b901ee775cfa
Darshit1009 <darshitkacha1009@gmail.com>
2026-06-01 11:17:06 +0530

    1920. Build Array from Permutation

---
+++ L_1920.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans.push_back(nums[nums[i]]);
+        }
+
+        return ans;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> ab = {1, 2, 3, 4, 5, 6, 7};
+    vector<int> asn;
+    asn = s.buildArray(ab);
+    for (int i = 0; i < asn.size(); i++)
+    {
+        cout << asn[i];
+    }
+    return 0;
+}
